f041cc91600a79989de171f34b33e6f20526203c
isnendyankp <isnendyankp@gmail.com>
2023-10-20 10:20:04 +0700

    recreate function  script for back to top

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementsByClassName("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -13,7 +13,7 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+const topFunction = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
